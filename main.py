dff76881035b720b6ef0c6fb76ee409c9e19609f
Максим <maxiim114@gmail.com>
2021-04-11 14:59:47 +0500

    pravo4ki

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-"""Case-study Бредогенератор
+"""
+
+Case-study Бредогенератор
 Разработчики:
-Бикметов Э.Б. = 40%, Бычков К.А. = , Кондрашов М.С. =
+Бикметов Э.Б. = 40%, Бычков К.А. = , Кондрашов М.С. = 35%
 
 """
+from random import *
+
+# Asking for a file name
 filename = input('Введите название файла (с расширением (.txt)) : ')
 with open(filename, 'r', encoding='utf-8') as f:
     list_string = f.readlines()
 txt = ''
+
+# deleting/fixing symbols
 for el in list_string:
     if el.find('\n') != -1:
         el = el[:el.find('\n')] + el[el.find('\n') + 1:] + ' '
@@ -20,16 +27,18 @@ for char in txt:
     if char in symbols and txt[txt.find(char) - 1] == ' ':
         txt = txt[:txt.find(char) - 1] + txt[txt.find(char):]
 
+# making words lists
 words_txt = txt.split()
 words_txt_2 = words_txt.copy()
 word_unique = []
 
+# unique words list
 for word in words_txt:
     if word not in word_unique:
         word_unique.append(word)
 
+# making dictionary
 dictionary = {}
-
 for word in word_unique:
     lst = []
     ind = words_txt_2.index(word)
@@ -45,13 +54,13 @@ for word in word_unique:
         lst = []
     dictionary[word] = lst
 
+# First symbol is upper list
 word_upper = []
 for el in words_txt:
     if el[0].isupper():
         word_upper.append(el)
 
-from random import *
-
+# Asking for a number of sentences & generating string
 num_sent = int(input('Введите колличество генерируемых предложений: '))
 for i in range(num_sent):
     w1 = choice(word_upper)
