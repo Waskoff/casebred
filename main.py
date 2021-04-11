1e850d45cc48e87cf8b8670b5492b404cf56e2be
Waskoff <73088341+Waskoff@users.noreply.github.com>
2021-04-11 13:56:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
-with open('input.txt', 'r', encoding='utf-8') as f:
+"""Case-study Бредогенератор
+Разработчики:
+Бикметов Э.Б. = 40%, Бычков К.А. = , Кондрашов М.С. =
+
+"""
+filename = input('Введите название файла (с расширением (.txt)) : ')
+with open(filename, 'r', encoding='utf-8') as f:
     list_string = f.readlines()
 txt = ''
 for el in list_string:
     if el.find('\n') != -1:
-        el = el[:el.find('\n')] + el[el.find('\n')+1:] + ' '
-    del_line = '@"#$%^&*()_-+=|/{}[]:;<>`~\''
+        el = el[:el.find('\n')] + el[el.find('\n') + 1:] + ' '
+    del_line = '@"#$%^&*()_—+=|/{}[]:;<>`~\'»«'
     for char in el:
         if char not in del_line:
             txt += char
+
 for char in txt:
     symbols = '?.,!'
-    if char in symbols and txt[txt.find(char)-1] == ' ':
-        txt = txt[:txt.find(char)-1] + txt[txt.find(char):]
+    if char in symbols and txt[txt.find(char) - 1] == ' ':
+        txt = txt[:txt.find(char) - 1] + txt[txt.find(char):]
+
 words_txt = txt.split()
 words_txt_2 = words_txt.copy()
 word_unique = []
+
 for word in words_txt:
     if word not in word_unique:
         word_unique.append(word)
+
 dictionary = {}
+
 for word in word_unique:
-    next_word = []
-    n = words_txt.count(word)
-    for num in range(n):
-        if words_txt.index(word, num)+1 < len(words_txt):
-            next_word.append(words_txt[words_txt.index(word, num)+1])
-            words_txt.remove(words_txt[words_txt.index(word)])
-    dictionary[word] = next_word
-print(dictionary)
+    lst = []
+    ind = words_txt_2.index(word)
+
+    while ind != -1:
+        try:
+            ind = words_txt_2.index(word) + 1
+            lst.append(words_txt_2[ind])
+            del words_txt_2[ind - 1]
+        except:
+            ind = -1
+    if word[-1] in '.!?':
+        lst = []
+    dictionary[word] = lst
 
 word_upper = []
-for el in words_txt_2:
+for el in words_txt:
     if el[0].isupper():
         word_upper.append(el)
-print(word_upper)
 
 from random import *
 
@@ -63,5 +78,3 @@ for i in range(num_sent):
         print(' '.join(sent) + '.')
     else:
         print(' '.join(sent) + '.')
-
-
